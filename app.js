e60e597a77f4a01012d328a3fe971e9e9ad8df47
DevMatch User <someone@devmatch.io>
2024-10-27 12:05:27 -0700

    revised app.js

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const express = require("express");
 const dogFacts = require("./dog_facts");
 const app = express();
 
-app.get("/facts", (req, res) => {
+app.get('/facts', (req, res) => {
     const { number } = parseInt(req.query.number);
-    if (number < 1) {
-        return res.status(400).json({message: "Invalid number parameter", success: false});
+    if (isNaN(number) || number < 1) {
+        return res.status(400).json({message: 'Invalid number parameter', success: false});
     }
     return res.json({facts: dogFacts.slice(0, number), success: true});
 });
 
 app.use((req, res) => {
-    res.status(404).json({message: "Not Found", success: false});
+    res.status(404).json({message: 'Not Found', success: false});
 });
 
 
